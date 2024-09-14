7e8f12d01b6ffe9cdc7415ef6dff766e7599083e
Tedtawut <vouspinktape@gmail.com>
2024-09-14 20:35:22 +0700

    finish

--- cell03/ex04/ex03bis/todo.js
+++ cell03/ex04/ex03bis/todo.js
@@ -1,22 +1,22 @@
 $(document).ready(function() {
-    // Function to add a new TO DO item
+
     function addTodo(text) {
         const $todoDiv = $('<div class="todo"></div>').text(text);
 
-        // Add click event to remove the TO DO
+
         $todoDiv.click(function() {
             if (confirm('Do you want to remove this TO DO?')) {
                 $(this).remove();
-                saveTodos(); // Save list after removal
+                saveTodos();
             }
         });
 
-        // Insert new TO DO at the top of the list
+
         $('#ft_list').prepend($todoDiv);
-        saveTodos(); // Save list after addition
+        saveTodos(); 
     }
 
-    // Function to save TO DOs to cookies
+
     function saveTodos() {
         const todos = [];
         $('.todo').each(function() {
@@ -25,7 +25,7 @@ $(document).ready(function() {
         document.cookie = `todos=${JSON.stringify(todos)};path=/;expires=Fri, 31 Dec 9999 23:59:59 GMT`;
     }
 
-    // Function to load TO DOs from cookies
+
     function loadTodos() {
         const cookies = document.cookie.split(';');
         let todosCookie = cookies.find(cookie => cookie.trim().startsWith('todos='));
@@ -38,16 +38,16 @@ $(document).ready(function() {
         }
     }
 
-    // Event listener for 'New' button to create a new TO DO
+ 
     $('#newButton').click(function() {
         const todoText = prompt('Enter a new TO DO:');
         if (todoText && todoText.trim() !== '') {
-            addTodo(todoText); // Add TO DO if it's not empty
+            addTodo(todoText); 
         } else {
             alert('The TO DO cannot be empty.');
         }
     });
 
-    // Load TO DOs when the page is loaded
+
     loadTodos();
 });
